039d25a2407f80f626833940a05b8bb0ca8eabcc
GrayOckham <jsdietrich@gmail.com>
2020-12-04 22:47:44 -0500

    Module 4 take 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
-import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd.js";
+import QuizQuestion from "./QuizQuestion.js";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = { quiz_position: 1 };
+    this.state = { quiz_position: 2 };
   }
 
   render() {
+    const isQuizEnd = true;
+    console.log(isQuizEnd, quizData.quiz_questions.length);
     const question = quizData.quiz_questions[this.state.quiz_position - 1];
-    return <QuizQuestion quiz_question={question} />;
+    return (
+      <div>
+        {isQuizEnd && <QuizEnd />}
+        <QuizQuestion quiz_question={question} />
+      </div>
+    );
   }
 }
 
