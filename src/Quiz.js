48df0699e4114c031ae388f05bf2e432a8f7794b
GrayOckham <jsdietrich@gmail.com>
2020-12-04 22:54:19 -0500

    Did stupid things to get tests to pass

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import QuizEnd from "./QuizEnd.js";
 import QuizQuestion from "./QuizQuestion.js";
+
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = { quiz_position: 2 };
+    this.state = { quiz_position: 1 };
   }
 
   render() {
-    const isQuizEnd = true;
-    console.log(isQuizEnd, quizData.quiz_questions.length);
-    const question = quizData.quiz_questions[this.state.quiz_position - 1];
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
     return (
       <div>
-        {isQuizEnd && <QuizEnd />}
-        <QuizQuestion quiz_question={question} />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
